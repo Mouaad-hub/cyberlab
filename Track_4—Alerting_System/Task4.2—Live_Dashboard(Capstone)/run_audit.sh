90ecd3c4236e3978d0fdcc49af6af3c9ab1d4f21
Mouaad ELAMLIH <elamlih.mouaad@gmail.com>
2026-06-08 13:16:07 +0100

    Update run_audit.sh

--- Track_4—Alerting_System/Task4.2—Live_Dashboard(Capstone)/run_audit.sh
+++ Track_4—Alerting_System/Task4.2—Live_Dashboard(Capstone)/run_audit.sh
@@ -5,25 +5,25 @@ today=$(date +"%Y-%m-%d")
 yesterday=$(date -d "-1 day" +"%Y-%m-%d")
 
 # Check if the audit script exists, then execute it
-if [ -f /home/mouaad/audit/audit.py ]; then 
-    sudo python3 /home/mouaad/audit/audit.py
+if [ -f /path/to//audit.py ]; then 
+    sudo python3 /path/to//audit.py
     # copy the today findings to the dashboard folder and change its name to latest_report.json
     sudo cp /var/reports/findings/findings_$today.json /path/to/dashboard/latest_report.json
     
     # Check if the diff report script and yesterday's findings exist, then execute diff generation
-    if [ -f /home/mouaad/audit/diff_reports.py ] && [ -f /var/reports/findings/findings_$yesterday.json ]; then 
-        sudo python3 /home/mouaad/audit/diff_reports.py /var/reports/findings/findings_$today.json /var/reports/findings/findings_$yesterday.json
+    if [ -f /path/to/diff_reports.py ] && [ -f /var/reports/findings/findings_$yesterday.json ]; then 
+        sudo python3 /path/to/diff_reports.py /var/reports/findings/findings_$today.json /var/reports/findings/findings_$yesterday.json
         # run the alter script to notify for any new HIGH findings 
-        sudo python3 /home/mouaad/audit/alerter.py /var/reports/diff/diff_report_$today.json /home/mouaad/audit/config_file.json
+        sudo python3 /path/to/alerter.py /var/reports/diff/diff_report_$today.json /path/to/config_file.json
     fi
     
     # Check if the report generator script exists
-    if [ -f /home/mouaad/audit/report_generater.py ]; then 
+    if [ -f /path/to/report_generater.py ]; then 
         # Run the report generator with or without the diff report based on its existence
         if [ -f /var/reports/diff/diff_report_$today.json ]; then 
-            sudo python3 /home/mouaad/audit/report_generater.py /var/reports/findings/findings_$today.json -d /var/reports/diff/diff_report_$today.json
+            sudo python3 /path/to/report_generater.py /var/reports/findings/findings_$today.json -d /var/reports/diff/diff_report_$today.json
         else  
-            sudo python3 /home/mouaad/audit/report_generater.py /var/reports/findings/findings_$today.json
+            sudo python3 /path/to/report_generater.py /var/reports/findings/findings_$today.json
         fi
     else 
         echo "can't find report_generater.py"
