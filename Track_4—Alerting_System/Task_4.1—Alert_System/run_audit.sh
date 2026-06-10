9a1f85fc734135c7c25f77655f3ee2dd981a748a
Mouaad ELAMLIH <elamlih.mouaad@gmail.com>
2026-06-10 15:50:20 +0100

    Update run_audit.sh

--- Track_4—Alerting_System/Task_4.1—Alert_System/run_audit.sh
+++ Track_4—Alerting_System/Task_4.1—Alert_System/run_audit.sh
@@ -11,8 +11,6 @@ if [ -f /path/to/audit.py ]; then
     # Check if the diff report script and yesterday's findings exist, then execute diff generation
     if [ -f /path/to/diff_reports.py ] && [ -f /var/reports/findings/findings_$yesterday.json ]; then 
         sudo python3 /path/to/diff_reports.py /var/reports/findings/findings_$today.json /var/reports/findings/findings_$yesterday.json
-        # run the alter script to notify for any new HIGH findings 
-        sudo python /path/to/alerter.py /var/reports/diff/diff_report_$today.json /path/to/config_file.json
     fi
     
     # Check if the report generator script exists
@@ -20,6 +18,8 @@ if [ -f /path/to/audit.py ]; then
         # Run the report generator with or without the diff report based on its existence
         if [ -f /var/reports/diff/diff_report_$today.json ]; then 
             sudo python3 /path/to/report_generater.py /var/reports/findings/findings_$today.json -d /var/reports/diff/diff_report_$today.json
+            # run the alter script to notify for any new HIGH findings 
+            sudo python /path/to/alerter.py /var/reports/diff/diff_report_$today.json /path/to/config_file.json
         else  
             sudo python3 /path/to/report_generater.py /var/reports/findings/findings_$today.json
         fi
